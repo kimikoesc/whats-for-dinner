33f43e020c8e6239ccb9e3c292865910651402a1
Kimiko <kimikoescovilla@gmail.com>
2021-10-18 20:30:09 +0900

    feat: setup firebase hosting.

---
+++ src/firebase-config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDEAHNzq0wk2Qw_IOQJR5F4N6zgBPDGkI0",
+  authDomain: "whats-for-dinner-0509.firebaseapp.com",
+  projectId: "whats-for-dinner-0509",
+  storageBucket: "whats-for-dinner-0509.appspot.com",
+  messagingSenderId: "1045937462808",
+  appId: "1:1045937462808:web:6b75a790e411fa0d212d6d"
+};
+
+const app = initializeApp(firebaseConfig);
