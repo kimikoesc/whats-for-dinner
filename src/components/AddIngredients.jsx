ef334fa87ac203880ba04eab12a2b5e57c051b3f
Kimiko <kimikoescovilla@gmail.com>
2022-01-06 13:04:09 +0900

    feat: improved add ingredients section.

--- src/components/AddIngredients.jsx
+++ src/components/AddIngredients.jsx
@@ -1,4 +1,4 @@
-import React, { createRef } from 'react'
+import React, { useState, createRef } from 'react'
 import store from '../store'
 import { doc, updateDoc, arrayUnion } from "@firebase/firestore";
 import db from "../firebase-config";
@@ -12,21 +12,40 @@ function AddIngredients(props) {
     const { username } = props;
     const textInput = createRef();
     const usernameRef = doc(db, 'Users', username);
+    const [added, setAdded] = useState(false);
+    const [invalid, setInvalid] = useState(false);
     
     const addToInventory = async () => {
-       store.dispatch({
-           type: "add",
-           item: textInput.current.value.toLowerCase()
-       })
-       await updateDoc(usernameRef, {
-           Inventory: arrayUnion(textInput.current.value)
-       })
+        if (textInput.current.value.length > 2) {
+            store.dispatch({
+                type: "add",
+                item: textInput.current.value.toLowerCase()
+            })
+            await updateDoc(usernameRef, {
+                Inventory: arrayUnion(textInput.current.value.toLowerCase())
+            })
+            setInvalid(false)
+            setAdded(true)
+        } else {
+            setInvalid(true)
+            setAdded(false)
+        }
     };
 
     return (
         <div>
             <input ref={textInput} type="text" placeholder="Enter an ingredient"/>
             <button onClick={addToInventory}> add </button>
+            { invalid ? 
+            <div className="add-error">
+                    <p> Please enter a valid ingredient. </p>
+            </div> 
+            : null }
+            { added ?
+            <div className="add-confirmed">
+                <p> Added! </p>
+            </div> 
+            : null }
         </div>
     )
 }
